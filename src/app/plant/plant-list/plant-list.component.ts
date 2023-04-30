97f0549c5038edf7243a1b2452bc5f8d23f37cc7
Camila Gómez <mc.gomezh1@uniandes.edu.co>
2023-04-29 19:05:27 -0500

    Call service function on plant list component

--- src/app/plant/plant-list/plant-list.component.ts
+++ src/app/plant/plant-list/plant-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PlantService } from '../plant.service';
+import { Plant } from '../plant';
 
 @Component({
   selector: 'app-plant-list',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlantListComponent implements OnInit {
 
-  constructor() { }
+  plants: Array<Plant> = [];
+  constructor(private plantService: PlantService) { }
 
   ngOnInit() {
+    this.getPlants();
+  }
+
+  getPlants(){
+    this.plantService.getPlants().subscribe(plants => {
+      this.plants = plants;
+    });
   }
 
 }
