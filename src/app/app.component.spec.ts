d98e707db6009c7b58c3571a08b00302f8567f9d
Camila Gómez <mc.gomezh1@uniandes.edu.co>
2023-04-29 21:10:41 -0500

    Create plantlistcomponent test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,22 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { PlantListComponent } from './plant/plant-list/plant-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { PlantService } from './plant/plant.service';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        HttpClientModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        PlantListComponent
       ],
+      providers: [ PlantService ]
     }).compileComponents();
   });
 
@@ -20,16 +26,4 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'evaluacion_practica_angular'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('evaluacion_practica_angular');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('evaluacion_practica_angular app is running!');
-  });
 });
--- src/app/plant/plant-list/plant-list.component.spec.ts
+++ src/app/plant/plant-list/plant-list.component.spec.ts
@@ -4,14 +4,20 @@ import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
 import { PlantListComponent } from './plant-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { PlantService } from '../plant.service';
+import { Plant } from '../plant';
 
 describe('PlantListComponent', () => {
   let component: PlantListComponent;
   let fixture: ComponentFixture<PlantListComponent>;
+  let debug: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PlantListComponent ]
+      imports: [ HttpClientModule],
+      declarations: [ PlantListComponent ],
+      providers: [ PlantService ]
     })
     .compileComponents();
   }));
@@ -19,10 +25,33 @@ describe('PlantListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(PlantListComponent);
     component = fixture.componentInstance;
+    for(let i = 0; i< 3; i++){
+      const plant = new Plant(
+        i,
+        "nombre",
+        "nombre cientifico",
+        "tipo",
+        1000,
+        "clima",
+        "sustrato"
+      )
+      component.plants.push(plant);
+    }
     fixture.detectChanges();
+    debug = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('There must be one table created', () => {
+    const table = debug.query(By.css('table'));
+    expect(table).toBeTruthy();
+  })
+
+  it('It should have 4 tr entries (1 header tr + 3 table entries)', () =>{
+    const trs = debug.queryAll(By.css('tr'));
+    expect(trs.length).toBe(4);
+  })
 });
